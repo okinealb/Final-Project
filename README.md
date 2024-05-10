04a78eab5c33186954cacf5bcc6ecdef09fcc6c7
Albert-Kenneth <albertjr329@gmail.com>
2024-05-10 04:18:42 -0500

    Finish Implementing Seam Carving

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Final-Project
 CSC-301 Analysis of Algorithms Final Project
 
-1. Store the image
-2. Add the image to the array
-3. make gif using stored images
+TODO:
+1. Format code as Juptyer notebook
--- Vertical-Seam/seam_carving.py
+++ Vertical-Seam/seam_carving.py
@@ -3,130 +3,83 @@ import numpy as np              # Importing the NumPy package
 from PIL import Image           # Importing Image from the PIL package
 
 # === Functions ===============================================================
-# Calculate the energy of the pixel (x,y) by considering the RGB values of the
-#   pixels immediately above/below and to the left/right.
-def calculate_energy(x, y):
-  print(x, y, len(image2), len(image2[0]))
+# Calculate and return the energy and cost of each pixel
+def calculate_energy(row: int, col: int, map: np.ndarray) -> float:
   # If the pixel is on the border, then the corresponding energy is 1000
-  if (x == 0 or y == 0 or x == cols-1 or y == rows-1): return 1000
-  # Otherwise, calculate the energy of the pixel (x,y) using the formula
+  if (col == 0 or row == 0 or col == cols-1 or row == rows-1): return 1000
+  # Otherwise, calculate the energy of the pixel using the formula given
   else:
     # Calculate the horizontal and vertical costs using the energy function
-    h_cost = np.subtract(image2[x-1, y], image2[x+1, y])
-    v_cost = np.subtract(image2[x, y-1], image2[x, y+1])
-    # Return the sqrt of the combined, squared horizontal and vertical costs 
-    return ((np.sum(np.square(h_cost)) + np.sum(np.square(v_cost))) ** 1/2)
+    h_cost = np.subtract(image_map[row][col-1], image_map[row][col+1], dtype=int)
+    v_cost = np.subtract(image_map[row-1][col], image_map[row+1][col], dtype=int)
+    # Return the sqrt of the combined, squared horizontal and vertical costs
+    return ((np.sum(np.square(h_cost)) + np.sum(np.square(v_cost))) ** (1/2))
   
-# Find the minimum cost of the possible pixels above the current pixel at (x,y)
-def min_cost(tbl, x, y):
-  # Determine where the pixel is relative to the border, changing according to
-  if   (y == 0): return 0                             # on top border
-  elif (x == 0): return min(tbl[y-1][x:x+2])          # on left border
-  elif (x == cols-1): return min(tbl[y-1][x-1:x+1])   # on right border
-  else: return min(tbl[y-1][x-1:x+2])                 # inside of border
+# Find the minimum cost of the possible pixels above the current pixel
+def min_cost(row: int, col: int, cst: np.ndarray) -> int:
+  # Determine where the pixel is relative to the border, then find the minimum
+  #   cost of the 3 possible pixels immediately above the current pixel
+  if   (row == 0): return 0                                  # on top border
+  elif (col == 0): return min(cst[row-1][col:col+2])         # on left border
+  elif (col == cols-1): return min(cst[row-1][col-1:col+1])  # on right border
+  else: return min(cst[row-1][col-1:col+2])                  # inside of border
   
 # Remove the pixel (row,col) by first coloring it red, then shifting all pixels
 #   after it forward and coloring the last remaining pixels black
-def remove_pixel(pixels, tbl, x, y):
-  color_red(pixels, x, y) 
-  # Shift the pixels after the initial pixel (col,row) forward
-  for col in range(x, cols): 
-    # Shift all pixels behind forward, then olor all remaining pixels black
-    pixels[col, y] =  pixels[col + 1, y] if (col < cols - 1) else (0, 0, 0)
-    tbl[y][col] = tbl[y][col + 1] if (col < cols - 1) else rows * 1000
-    
-# Indicate which pixels are on the seam by coloring the pixel (col,row) red
-def color_red(pixels, x, y): 
-  # Return the new pixel_map, with the pixel (col,row) colored
-  pixels[x, y] = (255, 0, 0)
-
-# Find the index of the minimum energy in the row
-def min_index(row):
-  # First find the minimum energy seam in the row
-  min_energy = min(energy_cst[row])
-  # Iterate through the energy table to find the index of the minimum
-  for col in range(cols):
-    if (energy_cst[row][col] == min_energy): return col
+def remove_pixel(row: int, col: int, map: np.ndarray, cst: np.ndarray) -> None:
+  # Shift the pixels after the initial pixel forward, excluding the pixel given
+  map[row][col:cols-1] = map[row][col+1:cols]; map[row][cols-1] = (0,0,0)
+  cst[row][col:cols-1] = cst[row][col+1:cols]; cst[row][cols-1] = rows*1000
     
 # Find the column index of the next pixel using the previous vertical seam
-def find_path(tbl, row, prev):
-  # Determine whether the previous seam is on the border or not
-  if (prev == 0):  
-    loc = np.where(tbl[row] == min(tbl[row][prev : prev+2]))
-  elif (prev == cols-1):
-    loc = np.where(tbl[row] == min(tbl[row][prev-1:prev+1]))
-  else: 
-    loc = np.where(tbl[row] == min(tbl[row][prev-1:prev+2]))
+def find_path(row: int, prev: int, cst: np.ndarray) -> int:
+  # Determine where the pixel is relative to the border
+  loc = (np.where(cst[row] == min(cst[row][prev : prev+2])) if (prev == 0)
+    else np.where(cst[row] == min(cst[row][prev-1:prev+1])) if (prev == cols-1)
+    else np.where(cst[row] == min(cst[row][prev-1:prev+2])))
   # Return the column index of the minimum pixel above the previous pixel
-  return find_nearest(loc[0], prev)
-    
-
-def find_nearest(arr, index): 
-  for i in range(len(arr)): 
-    if (abs(arr[i] - index) <= 1): return arr[i]
-  
+  return ([i for i in loc[0] if (abs(i - prev) <= 1)][0])
 
 # === Initialization ==========================================================
-# Read in the input image of the ocean
+# Prompt the user for the input image path and the output image dimensions
 file_path = "sample.jpg"
-
-# Prompt the user for the input image path and the dimensions of the output
-#   image, then store them
-# NOTE: currently does not prompt user, nor find seams based on dimensions
-output_width = 408
-#output_w = int(input("Output width: "))    # Translate the output width into an int
-
-image = Image.open(file_path) # Import the image from the given filepath
-image2=np.array(image).astype(int)
-#pixel_map = image.load()      # Extract the pixel map from input
+output_width = int(input("Output width: "))
+# Use input to import the image, and extract the pixel map as an np array
+image = Image.open(file_path)
+image_map = np.array(image)
 
 # Initialize cols, rows to the size of the image (width, height, respectively)
-rows,cols,rgb = image2.shape
+rows, cols = image_map.shape[:2]
 # Initialize arrays to store energy, table, and seam information into
 seam = np.zeros(rows, dtype=int)
-energy_tbl = np.zeros((rows, cols), dtype=int)
-energy_cst = np.zeros((rows, cols), dtype=int)
+energy_tbl = np.zeros((rows, cols))
+energy_cst = np.zeros((rows, cols))
+# Create output image array to populate later
+image_out = np.zeros((rows, output_width, 3), dtype=np.uint8) 
 
-# === Construct Energy Costs and Tables =======================================
+# === Construct Energy Table and Cost =======================================--
 # Iterate through each pixel in the image, computing the energy of the pixel
 for row in range(rows):
   for col in range(cols):
-    # Calculate the energy of the pixel (col,row), then update the energy arrays
-    energy_tbl[row][col] = calculate_energy(row, col)
-    print(calculate_energy(col,row))
-    
-    energy_cst[row][col] = min_cost(energy_cst, col, row) + energy_tbl[row][col]
-    
-energy_output=energy_tbl[1:rows-1,1:cols-1]
-#print(energy_output)
-np.savetxt("energy.csv", energy_output, delimiter=",")
-#(energy_tbl).tocsv("energy.csv") # Write pixel energies
+    # Calculate the energy of the pixel, then update the energy arrays
+    energy_tbl[row][col] = calculate_energy(row, col, image_map)
+    energy_cst[row][col] = min_cost(row, col, energy_cst) + energy_tbl[row][col]
 
 # === Carve Seams =============================================================
 # do better update image instead of moving pixel
-for i in range(200):
+for i in range(cols - output_width):
   for row in reversed(range(rows)):
-    if (row == rows-1):
-      # Find the current minimum vertical seam in the picture
-      # NOTE: np.where is made for 2d arrays, (but might find the wrong pixel
-      #   column if we use the entire array) so getting the column index is a 
-      #   little weird and requires indexing into the result
-      seam[row] = np.where(energy_cst[rows-1] == min(energy_cst[rows-1]))[0][0]
-    else:
-      # Follow the path of the minimum vertical seam, from bottom up
-      seam[row] = find_path(energy_cst, row, seam[row+1])
+    # Follow the path of the minimum vertical seam, from bottom up
+    seam[row] = ((np.where(energy_cst[rows-1] == min(energy_cst[rows-1]))[0][0])
+                if row == rows-1 else (find_path(row, seam[row+1], energy_cst)))
     # Remove the pixel (by moving all pixels behind it forward one place)
-    color_red(pixel_map, seam[row].item(), row)
-  #image.save(f"Image{i}.jpg")
-  for row in reversed(range(rows)):
-    remove_pixel(pixel_map, energy_cst, seam[row].item(), row)
-  #image.save(f"Image{i}.jpg")
-
+    remove_pixel(row, seam[row], image_map, energy_cst)
+  # Save and output the minimum energy seam to an external csv file
+  if (i == 0): np.savetxt("seam1.csv", seam, delimiter=",")
 
 # === Termination =============================================================
-# Save and output the energies, smallest weight seam, and final image
-np.savetxt("seam1.csv", seam, delimiter=",")
-#(seam).to_csv("seam1.csv") # Write the smallest seam
-image.save("FinalImage.jpg") # Save the output image, with the seams removed
-
-#print(seam)
+# Update the rows in the output image with the rows from the original image
+for i in range(rows): image_out[i] = image_map[i][:output_width]
+# Save and output image, with each of the seams removed to fit the output width
+np.savetxt("energy.csv", energy_tbl[1:rows-1, 1:cols-1], delimiter=",")
+(Image.fromarray(image_out)).save("FinalImage.jpg") # Save the new image
